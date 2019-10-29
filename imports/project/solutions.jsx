d4b940efcb92e5a23e2ab2e911873fa62a67eaee
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-10-29 21:16:30 +0300

    initial

---
+++ imports/project/solutions.jsx
@@ -0,0 +1,55 @@
+// @flow
+
+import React, { useState, useEffect } from 'react';
+
+import { makeStyles, Grid, TextField, LinearProgress, List, ListItem, ListItemText } from '@material-ui/core';
+import { gql, useGql, useQuery } from '../packages/gql/use';
+
+const QUERY = gql`
+  query QUERY($value: String) {
+    symptoms(where: {value: {_like: $value}}) {
+      id
+      value
+      solutions {
+        id
+        value
+      }
+    }
+  }
+
+`;
+
+export const Solutions = ({ symptom }: { symptom: string }) => {
+  const { data, loading, error, refetch } = useQuery(QUERY, {
+    variables: { value: `%${symptom.trim()}%` },
+  });
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    setTime(setTimeout(() => {
+      setTime(null);
+      refetch({ value: `%${symptom.trim()}%` });
+    }, 1000));
+  }, [symptom]);
+
+  return <>
+    {time && loading ? <LinearProgress /> : <LinearProgress variant="determinate" value={0}/>}
+    {!!error && <pre>{error}</pre>}
+    {!!symptom.trim() && <>
+      <List>
+        {!!data && <>
+          {data.symptoms.map(symptom => (<React.Fragment key={symptom.id}>
+            <ListItem key={symptom.id} disabled>
+              <ListItemText>{symptom.value}</ListItemText>
+            </ListItem>
+            {symptom.solutions.map(solution => (
+              <ListItem key={solution.id}>
+                <ListItemText>{solution.value}</ListItemText>
+              </ListItem>
+            ))}
+          </React.Fragment>))}
+        </>}
+      </List>
+    </>}
+  </>;
+};
